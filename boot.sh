0095d45ca96bdf699e88b70d3aad6f68c62def09
80036nd <40246027+80036nd@users.noreply.github.com>
2020-12-31 03:07:47 -0500

    Delete boot.sh

--- boot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mv ~/Documents/iDroid-Installer/checkra1n.file ~/Documents/iDroid-Installer/checkra1n.app
-chmod 777 ~/Documents/iDroid-Installer/checkra1n.app/Contents/MacOS/checkra1n
-chmod 777 ~/Documents/iDroid-Installer/load-linux.mac
-chmod 777 ~/Documents/iDroid-Installer/start_mac.sh
-./Documents/iDroid-Installer/start_mac.sh
